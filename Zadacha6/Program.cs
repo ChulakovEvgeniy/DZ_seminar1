aa4b1ab16445ca5a95a5ceeacb37e91b735ff978
«ChulakovEvgeniy» <>
2023-03-27 12:31:11 +0300

    Zadacha 6 решена

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число = ");
+string per = Console.ReadLine();
+int num = Convert.ToInt32(per);
+
+if (num % 2 == 0) {Console.WriteLine ($"Число {num} четное");}
+else {
+    Console.WriteLine ($"Число {num} нечетное");
+}
